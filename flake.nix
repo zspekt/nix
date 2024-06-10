b0bc66d7cfbdc602296d11276100660fd835b20d
zspekt <zspekt@tutanota.com>
2024-06-10 03:01:41 +0000

    formatted all nix files
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- flake.nix
+++ flake.nix
@@ -1,29 +1,29 @@
 {
-	description = "desktop conf";
+  description = "desktop conf";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-24.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = {nixpkgs, home-manager, ... }:
-	let 
-		system = "x86_64-linux";
-	in {
-	# nixosConfigurations.<hostnameDefinedOnConfiguration.nix>
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			inherit system;
-			modules = [ ./configuration.nix ];
-		};
-		homeConfigurations.zspekt = home-manager.lib.homeManagerConfiguration {
-			pkgs = nixpkgs.legacyPackages.${system};
-			modules = [ ./home.nix ];
-		};
-	};
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      # nixosConfigurations.<hostnameDefinedOnConfiguration.nix>
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+      homeConfigurations.zspekt = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./home.nix ];
+      };
+    };
 }
-
-
--- home.nix
+++ home.nix
@@ -1,10 +1,16 @@
-{config, pkgs, home-manager, ... }: {
-	home = {
+{
+  config,
+  pkgs,
+  home-manager,
+  ...
+}:
+{
+  home = {
 
-	username = "zspekt";
-	homeDirectory = "/home/zspekt";
-	stateVersion = "23.11";
-	};
+    username = "zspekt";
+    homeDirectory = "/home/zspekt";
+    stateVersion = "23.11";
+  };
 
   programs.git = {
     enable = true;
@@ -16,13 +22,15 @@
     };
   };
 
-programs.gpg = {
+  programs.gpg = {
     enable = true;
     publicKeys = [
-      { source = ./dotfiles/key.pub; trust = 5; }
+      {
+        source = ./dotfiles/key.pub;
+        trust = 5;
+      }
     ];
   };
-
 
   services.gpg-agent = {
     enable = true;
@@ -36,104 +44,99 @@ programs.gpg = {
     enableScDaemon = true;
   };
 
-# 	programs.zsh = {
-# 		enable = true;
-# 		syntaxHighlighting.enable = true;
-# 		shellAliases = {
-# 			mynixxa = "neofetch";
-# 		};
-#
-#     # antidote
-#     antidote = {
-#       enable = true;
-#
-#       plugins = [
-#
-#         "romkatv/zsh-defer"
-#         "ohmyzsh/ohmyzsh path:lib"
-#         "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
-#         "ohmyzsh/ohmyzsh path:plugins/git kind:defer"
-# #ohmyzsh/ohmyzsh path:plugins/git-extras kind:defer"
-# #ohmyzsh/ohmyzsh path:plugins/git-prompt kind:defer"
-#         "ohmyzsh/ohmyzsh path:plugins/golang"
-#         "ohmyzsh/ohmyzsh path:plugins/python kind:defer"
-#         "ohmyzsh/ohmyzsh path:plugins/gh kind:defer"
-#         "ohmyzsh/ohmyzsh path:plugins/alias-finder"
-# # ohmyzsh/ohmyzsh path:plugins/podman"
-#         "ohmyzsh/ohmyzsh path:plugins/docker"
-#         "ohmyzsh/ohmyzsh path:plugins/docker-compose"
-#         "ohmyzsh/ohmyzsh path:plugins/docker-machine"
-#         "zsh-users/zsh-completions"
-#         "ohmyzsh/ohmyzsh path:plugins/fzf"
-# # ohmyzsh/ohmyzsh path:plugins/zsh-interactive-cd"
-#         "romkatv/powerlevel10k"
-# # suggests pkg to install if command not found
-#         "ohmyzsh/ohmyzsh path:plugins/command-not-found"
-#         "zsh-users/zsh-autosuggestions kind:defer"
-#         "zsh-users/zsh-syntax-highlighting kind:defer"
-#         "zsh-users/zsh-history-substring-search kind:defer"
-#       ];
-#     };
-# 	};
-
-  
+  # 	programs.zsh = {
+  # 		enable = true;
+  # 		syntaxHighlighting.enable = true;
+  # 		shellAliases = {
+  # 			mynixxa = "neofetch";
+  # 		};
+  #
+  #     # antidote
+  #     antidote = {
+  #       enable = true;
+  #
+  #       plugins = [
+  #
+  #         "romkatv/zsh-defer"
+  #         "ohmyzsh/ohmyzsh path:lib"
+  #         "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
+  #         "ohmyzsh/ohmyzsh path:plugins/git kind:defer"
+  # #ohmyzsh/ohmyzsh path:plugins/git-extras kind:defer"
+  # #ohmyzsh/ohmyzsh path:plugins/git-prompt kind:defer"
+  #         "ohmyzsh/ohmyzsh path:plugins/golang"
+  #         "ohmyzsh/ohmyzsh path:plugins/python kind:defer"
+  #         "ohmyzsh/ohmyzsh path:plugins/gh kind:defer"
+  #         "ohmyzsh/ohmyzsh path:plugins/alias-finder"
+  # # ohmyzsh/ohmyzsh path:plugins/podman"
+  #         "ohmyzsh/ohmyzsh path:plugins/docker"
+  #         "ohmyzsh/ohmyzsh path:plugins/docker-compose"
+  #         "ohmyzsh/ohmyzsh path:plugins/docker-machine"
+  #         "zsh-users/zsh-completions"
+  #         "ohmyzsh/ohmyzsh path:plugins/fzf"
+  # # ohmyzsh/ohmyzsh path:plugins/zsh-interactive-cd"
+  #         "romkatv/powerlevel10k"
+  # # suggests pkg to install if command not found
+  #         "ohmyzsh/ohmyzsh path:plugins/command-not-found"
+  #         "zsh-users/zsh-autosuggestions kind:defer"
+  #         "zsh-users/zsh-syntax-highlighting kind:defer"
+  #         "zsh-users/zsh-history-substring-search kind:defer"
+  #       ];
+  #     };
+  # 	};
 
   home.file = {
     neovim = {
-         recursive = true;
-         source  = ./dotfiles/.config/nvim;
-         target = "./.config/nvim";
-       };
+      recursive = true;
+      source = ./dotfiles/.config/nvim;
+      target = "./.config/nvim";
+    };
 
-       zshPluginList = {
-         recursive = false;
-         source  = ./dotfiles/.zsh/zsh_plugins.txt;
-         target = "./.zsh/zsh_plugins.txt";
-       };
+    zshPluginList = {
+      recursive = false;
+      source = ./dotfiles/.zsh/zsh_plugins.txt;
+      target = "./.zsh/zsh_plugins.txt";
+    };
 
-       zshrc = {
-         recursive = false;
-         source  = ./dotfiles/.zshrc;
-         target = "./.zshrc";
-       };
+    zshrc = {
+      recursive = false;
+      source = ./dotfiles/.zshrc;
+      target = "./.zshrc";
+    };
 
-       p10k = {
-         recursive = false;
-         source  = ./dotfiles/.p10k.zsh;
-         target = "./.p10k.zsh";
-       };
+    p10k = {
+      recursive = false;
+      source = ./dotfiles/.p10k.zsh;
+      target = "./.p10k.zsh";
+    };
 
-       hypr = {
-         recursive = true;
-         source  = ./dotfiles/.config/hypr;
-         target = ".config/hypr";
-       };
+    hypr = {
+      recursive = true;
+      source = ./dotfiles/.config/hypr;
+      target = ".config/hypr";
+    };
 
-      tmux = {
-        recursive = false;
-        source  = ./dotfiles/.config/tmux/tmux.conf;
-        target = ".config/tmux/tmux.conf";
-      };
+    tmux = {
+      recursive = false;
+      source = ./dotfiles/.config/tmux/tmux.conf;
+      target = ".config/tmux/tmux.conf";
+    };
 
-       waybar = {
-         recursive = true;
-         source  = ./dotfiles/.config/waybar;
-         target = ".config/waybar";
-       };
+    waybar = {
+      recursive = true;
+      source = ./dotfiles/.config/waybar;
+      target = ".config/waybar";
+    };
 
-       kitty = {
-         recursive = true;
-         source  = ./dotfiles/.config/kitty;
-         target = ".config/kitty";
-       };
+    kitty = {
+      recursive = true;
+      source = ./dotfiles/.config/kitty;
+      target = ".config/kitty";
+    };
 
-
-       rofi = {
-         recursive = true;
-         source  = ./dotfiles/.config/rofi;
-         target = ".config/rofi";
-};
-};
-
+    rofi = {
+      recursive = true;
+      source = ./dotfiles/.config/rofi;
+      target = ".config/rofi";
+    };
+  };
 }
-
