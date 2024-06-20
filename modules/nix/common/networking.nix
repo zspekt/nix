d3a78dc39ebb271b5e1aa023bbc0ed6696af7f70
zspekt <zspekt@tutanota.com>
2024-06-20 13:22:54 -0300

    nix
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- modules/nix/common/networking.nix
+++ modules/nix/common/networking.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ hostname, ... }:
 {
   networking = {
+    hostName = "${hostname}";
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
     # useDHCP = true;
