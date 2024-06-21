48a1bc1d6f045f4e6bb3cec8af9d6a35a871f46b
zspekt <zspekt@tutanota.com>
2024-06-21 13:48:00 -0300

    networkmanager with iwd as a backend
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- modules/nix/common/networking.nix
+++ modules/nix/common/networking.nix
@@ -2,7 +2,11 @@
 {
   networking = {
     hostName = "${hostname}";
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    networkmanager = {
+      enable = true; # Easiest to use and most distros use this by default.
+      wifi.backend = "iwd";
+      dhcp = "dhcpcd";
+    };
 
     # useDHCP = true;
     # wireless.iwd.enable = true;
