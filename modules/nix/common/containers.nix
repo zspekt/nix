f2ee970cb966b27975eda53773bd6309e849c482
zspekt <zspekt@tutanota.com>
2024-07-01 21:41:00 -0300

    containers
    Signed-off-by: zspekt <zspekt@tutanota.com>

---
+++ modules/nix/common/containers.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }: # gracias contre
+{
+  # Enable common container config files in /etc/containers
+  virtualisation = {
+    oci-containers.backend = "podman";
+    podman = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        flags = [ "-af" ];
+      };
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+
+      # podman.extraPackages = [ pkgs.zfs ];
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
+    containers = {
+      enable = true;
+      # storage.settings = {
+      #   storage.driver = "zfs";
+      #   storage.graphroot = "/var/lib/containers/storage";
+      #   storage.runroot = "/run/containers/storage";
+      #   storage.options.zfs.fsname = "zroot/podman";
+      # };
+      containersConf.settings = {
+        containers.dns_servers = [ "8.8.8.8" "8.8.4.4" ];
+      };
+    };
+  };
+
+
+  # Useful otherdevelopment tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    # docker-compose # start group of containers for dev
+    podman-compose # start group of containers for dev
+  ];
+}
