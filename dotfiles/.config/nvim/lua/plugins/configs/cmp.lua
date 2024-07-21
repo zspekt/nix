72b6a713bd0eb9723e76695cacddfbd4348e2abd
zspekt <zspekt@tutanota.com>
2024-07-21 13:40:32 -0300

    nvim: fixed redundancy on cmp.lua (creating a table and then returning an anonymous table)
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.config/nvim/lua/plugins/configs/cmp.lua
+++ dotfiles/.config/nvim/lua/plugins/configs/cmp.lua
@@ -1,35 +1,9 @@
 local cmp = require "cmp"
 
-  local lspkind = require "lspkind"
-  lspkind.init {}
+local lspkind = require "lspkind"
+lspkind.init {}
 
 local options = {
-  completion = {
-    completeopt = "menu,menuone,noselect",
-  },
-
-
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-
-  mapping = {
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-y>"] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { "i", "c" }
-    ),
-    ["<C-]>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-  },
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
@@ -37,20 +11,14 @@ local options = {
     { name = "nvim_lua" },
     { name = "path" },
   },
-}
 
-return {
-  sources = {
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "nvim_lua" },
-      { name = "path" },
+  completion = {
+    completeopt = "menu,menuone,noselect",
   },
 
   window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 
   snippet = {
@@ -58,6 +26,7 @@ return {
       require("luasnip").lsp_expand(args.body)
     end,
   },
+
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -74,3 +43,5 @@ return {
     ["<C-e>"] = cmp.mapping.close(),
   },
 }
+
+return options
