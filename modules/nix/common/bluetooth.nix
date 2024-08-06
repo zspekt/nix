422052f9aee135057a963041b584490e19d88806
zspekt <zspekt@tutanota.com>
2024-08-06 17:07:16 -0300

    nix: added bluetooth
    Signed-off-by: zspekt <zspekt@tutanota.com>

---
+++ modules/nix/common/bluetooth.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+
+  environment.systemPackages = with pkgs; [
+    # blueman
+    bluez
+    bluez5-experimental
+  ];
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = false;
+      settings = {
+        General = {
+          # # make Xbox Series X controller work
+          # Class = "0x000100";
+          # ControllerMode = "bredr";
+          # FastConnectable = true;
+          # JustWorksRepairing = "always";
+          # Privacy = "device";
+          # Enable = "Source,Sink,Media,Socket"; 
+          Enable = "Sink,Media,Socket"; # Don't enable Source since I don't want bluetooth mic to be use
+          AutoEnable = true;
+          ControllerMode = "bredr";
+          Experimental = true;
+        };
+      };
+    };
+  };
+}
