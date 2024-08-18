580077ed83e38ec56f1d9be25d978a55af0682f0
zspekt <zspekt@tutanota.com>
2024-08-18 10:37:34 -0300

    nix: removed local hosts entry (router takes care of that)
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- modules/nix/common/networking.nix
+++ modules/nix/common/networking.nix
@@ -7,15 +7,6 @@
     useDHCP = true;
     useNetworkd = true;
     networkmanager.enable = false;
-    hosts = {
-      # "192.168.49.2" = [
-      #   "synchat.internal"
-      #   "synchatapi.internal"
-      # ];
-      #
-      "192.168.1.244" = [ "nixth.lan" ];
-      "192.168.1.128" = [ "nixpi.lan" ];
-    };
   };
 
   environment.systemPackages = with pkgs; [
