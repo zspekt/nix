7bc0b49f3ffd1c663e97cc1db188971dc265fdfb
zspekt <zspekt@tutanota.com>
2024-08-29 18:02:09 -0300

    nvim: treesitter-objects, demicolon, eyeliner working
    Signed-off-by: zspekt <zspekt@tutanota.com>

---
+++ dotfiles/.config/nvim/lua/plugins/treedemiliner.lua
@@ -0,0 +1,81 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        textobjects = {
+          move = {
+            enable = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]a"] = "@argument.outer",
+              ["]m"] = "@method.outer",
+              -- ...
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[a"] = "@argument.outer",
+              ["[m"] = "@method.outer",
+              -- ...
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    'jinh0/eyeliner.nvim',
+    keys = { 't', 'f', 'T', 'F' },
+    opts = {
+      highlight_on_key = true,
+      dim = true,
+      default_keymaps = false,
+    }
+  },
+  {
+    'mawkler/demicolon.nvim',
+    dependencies = {
+      'jinh0/eyeliner.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects'
+    },
+    keys = { ';', ',', 't', 'f', 'T', 'F', ']', '[', ']d', '[d' },
+    config = function()
+      require('demicolon').setup({
+        keymaps = {
+          horizontal_motions = false,
+          integrations = {
+            -- Integration with https://github.com/lewis6991/gitsigns.nvim
+            gitsigns = {
+              enabled = true,
+              keymaps = {
+                next = ']g',
+                prev = '[g',
+              },
+            },
+          },
+
+
+        },
+      })
+
+      local function eyeliner_jump(key)
+        local forward = vim.list_contains({ 't', 'f' }, key)
+        return function()
+          require('eyeliner').highlight({ forward = forward })
+          return require('demicolon.jump').horizontal_jump(key)()
+        end
+      end
+
+      local nxo = { 'n', 'x', 'o' }
+      local opts = { expr = true }
+
+      vim.keymap.set(nxo, 'f', eyeliner_jump('f'), opts)
+      vim.keymap.set(nxo, 'F', eyeliner_jump('F'), opts)
+      vim.keymap.set(nxo, 't', eyeliner_jump('t'), opts)
+      vim.keymap.set(nxo, 'T', eyeliner_jump('T'), opts)
+    end
+  }
+}
