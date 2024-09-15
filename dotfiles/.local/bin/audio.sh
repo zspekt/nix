4c977d4db9abb7732ab86340afbcbd1fcb50c94c
zspekt <zspekt@tutanota.com>
2024-09-15 04:24:31 -0300

    hypr: swaylock
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.local/bin/audio.sh
+++ dotfiles/.local/bin/audio.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 choices=$(pactl list sinks short | awk '{ print $2 }')
-var=$(echo -e "$choices" | rofi -format 'd' -dmenu -i)
+var=$(echo -e "$choices" | rofi -format 'd' -dmenu -i -p "Sinks 󰋋 :")
 
 exec pactl set-default-sink "$var"
---
+++ dotfiles/.local/bin/swaylock_dpms.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+swaylock &
+sleep 1
+hyprctl dispatch dpms off
