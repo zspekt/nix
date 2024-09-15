243069a914611b07d05b98e0ca824fb3debbce7b
zspekt <zspekt@tutanota.com>
2024-09-15 10:34:12 -0300

    nvim: i hope this makes treesitter shut the fuck up
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPost", "BufNewFile" },
 		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
+		-- build = ":TSUpdate",
     dependencies = "windwp/nvim-ts-autotag",
 		opts = function()
 			return require("plugins.configs.treesitter")
