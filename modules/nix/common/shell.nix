02e52e4ff03a30f836d538b1a1f6dc993764b84b
zspekt <zspekt@tutanota.com>
2024-09-24 11:36:46 -0300

    nix: back to using nixpkgs' tmuxinator
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- modules/nix/common/shell.nix
+++ modules/nix/common/shell.nix
@@ -53,8 +53,8 @@
 
     # tmux
     inputs.tmux-latest.defaultPackage.x86_64-linux # master
-    inputs.tmuxinator-flake.defaultPackage.x86_64-linux # 3.3.0 because idk how to deal with ruby versions
     inputs.sesh-latest.defaultPackage.x86_64-linux # master
+    tmuxinator
     thumbs
 
     # finding stuff
