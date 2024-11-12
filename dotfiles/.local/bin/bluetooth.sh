1d49cf42f7b15b56b379ac68ae09ee943a8b90c2
zspekt <zspekt@tutanota.com>
2024-11-12 00:29:02 -0300

    scripts: added 'bluetoothctl power on' to bluetooth.sh
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.local/bin/bluetooth.sh
+++ dotfiles/.local/bin/bluetooth.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bluetoothctl connect "$(bluetoothctl devices | awk '{mac=$2; $1=$2=""; gsub(/^[ \t]+|[ \t]+$/, ""); print "󰂯 " $0 " - " mac }' | rofi -dmenu -i -p "Bluetooth 󰂰 : " -fn "JetBrainsMono Nerd Font" -l 10 | awk -F " - " '{print $2}')"
+bluetoothctl power on && bluetoothctl connect "$(bluetoothctl devices | awk '{mac=$2; $1=$2=""; gsub(/^[ \t]+|[ \t]+$/, ""); print "󰂯 " $0 " - " mac }' | rofi -dmenu -i -p "Bluetooth 󰂰 : " -fn "JetBrainsMono Nerd Font" -l 10 | awk -F " - " '{print $2}')"
