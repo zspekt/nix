5ecd0d40e70c841a06bd0cce9b5c47d9996ffb45
zspekt <zspekt@tutanota.com>
2024-11-14 12:36:09 -0300

    nvim: pinned ibl to old version
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.config/nvim/lua/plugins/indent-blankline.lua
+++ dotfiles/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,11 +2,14 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
+    commit = "3af6493bf69e4a857a8b1fab36f333629d413a18",
+    tag = "v3.8.1",
     ---@module "ibl"
     ---@type ibl.config
     opts = {
       indent = {
-        char = '│',
+        char = { "│" },
+        tab_char = { "│" },
       },
       scope = { enabled = false, }
     },
