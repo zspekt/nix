e40a3a61bbe424f80184be0b24254ff93f9bd73b
zspekt <zspekt@tutanota.com>
2024-12-31 03:20:28 -0300

    nvim: hardtime ignore left and right arrowkeys (use em to switch buffers)
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- dotfiles/.config/nvim/lua/plugins/hardtime.lua
+++ dotfiles/.config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,16 @@ return {
   {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {}
+    opts = {
+      disabled_keys = { -- put keys here so hardtime won't get in the way
+        -- ["<Up>"] = {},  
+        -- ["<Down>"] = {},
+        ["<Left>"] = {}, -- currently using these 2 to switch buffers
+        ["<Right>"] = {},
+      },
+    },
+    config = function (_, opts)
+      require('hardtime').setup(opts)
+    end
   },
 }
