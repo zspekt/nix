7ed8c2e2ec3e104339b7622a5b0a5a9f91ac8d4c
zspekt <zspekt@tutanota.com>
2025-02-24 09:04:17 -0300

    nix: cleaned up dotlinks for mac
    Signed-off-by: zspekt <zspekt@tutanota.com>

--- modules/home-manager/users/zspekt/dotlinks.nix
+++ modules/home-manager/users/zspekt/dotlinks.nix
@@ -1,34 +1,5 @@
-{ hostname, ... }:
+{ ... }:
 {
-
-  imports =
-
-    if "${hostname}" == "nixos" then
-      [
-        ./dotlinks/hyprDesktop.nix
-        ./dotlinks/waybarDesktop.nix
-        ./dotlinks/kitty.nix
-        ./dotlinks/rofi.nix
-        ./dotlinks/mako.nix
-      ]
-    else if "${hostname}" == "nixth" then
-      [
-        ./dotlinks/hyprThpad.nix
-        ./dotlinks/waybarThpad.nix
-        ./dotlinks/kitty.nix
-        ./dotlinks/rofi.nix
-        ./dotlinks/mako.nix
-      ]
-    else if "${hostname}" == "nixpi" then
-      [ ]
-    else
-      throw "unknown host";
-
-  # source defines the path within the nix store
-  # from the perspective of the file.
-  # whereas target defines the path within home where we want the links
-  # when linking a dir, the whole dir will be read-only — not just its contents
-
   home.file =
     let
       homeDir = ../../../../dotfiles;
@@ -68,26 +39,7 @@
       tmuxTpm = {
         recursive = true;
         source = homeDir + /.tmux/plugins/tpm;
-        target = ".tmux/plugins/tpm";
-      };
-
-      tmuxSesh = {
-        recursive = true;
-        source = configDir + /sesh;
-        target = "./.config/sesh";
-      };
-
-      tmuxinator = {
-        recursive = true;
-        source = configDir + /tmuxinator;
-        target = "./.config/tmuxinator";
-      };
-
-      scripts = {
-        recursive = true;
-        source = homeDir + /.local/bin;
-        target = "./.local/bin";
-        executable = true;
+        target = ".config/tmux/plugins/tpm";
       };
     };
 }
